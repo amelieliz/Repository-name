cd255367391a173367e1a2d908d9615cec2eb2f0
amelieliz <ameliaryb6@gmail.com>
2026-01-04 06:01:47 +0300

    Лабораторная работа 4

---
+++ ЛР4_1.py
@@ -0,0 +1,18 @@
+# TODO решите задачу
+import json
+
+def task() -> float:
+
+    with open('input.json') as f:
+        data = json.load(f)
+
+
+    total = sum(item["score"] * item["weight"] for item in data)
+
+
+    return round(total, 3)
+
+print(task())
+
+
+
