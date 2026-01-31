abd198fe4692892c2e415f9686aa80a029be3eae
amelieliz <ameliaryb6@gmail.com>
2026-01-31 21:41:59 +0300

    Лабораторная работа 2

---
+++ лр2.1.py
@@ -0,0 +1,18 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+months = 0
+balance = money_capital
+while True:
+    balance += salary
+    expenses = spend * (1 + increase) ** months
+
+    if balance >= expenses:
+        balance -= expenses
+        months += 1
+    else:
+        break
+# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
+
+print("Количество месяцев, которое можно протянуть без долгов:", months)
